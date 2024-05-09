de8f30cb5a6f881b027ebbb53b606565367c8241
Ivelin <mihajlov.ivelin@gmail.com>
2024-05-09 18:06:34 +0300

    created a function to calc square perimeter

---
+++ dr8/index.js
@@ -0,0 +1,8 @@
+/* Task 1: Create a function to calculate the perimeter of a square
+Objective: To reinforce understanding of JavaScript functions by implementing a function to calculate the perimeter of a square.
+Write a JavaScript function named calculateSquarePerimeter that takes one parameter: 
+sideLength (the length of a side of the square). */
+function calculateSquarePerimeter(sideLength) {
+    return sideLength * 4;
+}
+console.log('The perimeter of the square is '+calculateSquarePerimeter(5));; // Expected output: 20
