8ee04005426bcf22e0098fb82e994774a6cfb530
Ivelin <mihajlov.ivelin@gmail.com>
2024-05-09 18:07:00 +0300

    created a function to calc farenheit based on given celcius

--- dr8/index.js
+++ dr8/index.js
@@ -6,3 +6,11 @@ function calculateSquarePerimeter(sideLength) {
     return sideLength * 4;
 }
 console.log('The perimeter of the square is '+calculateSquarePerimeter(5));; // Expected output: 20
+
+/* Task 2: Create a function to convert temperature from Celsius to Fahrenheit
+Objective: To practice JavaScript function creation by implementing a function to convert temperature from Celsius to Fahrenheit.
+Define a JavaScript function named celsiusToFahrenheit that takes one parameter: celsius (the temperature in Celsius). */
+function celsiusToFahrenheit(celsius) {
+    return celsius * 9/5 + 32;
+}
+console.log('The temperature in Fahrenheit is '+celsiusToFahrenheit(30)); // Expected output: 86
