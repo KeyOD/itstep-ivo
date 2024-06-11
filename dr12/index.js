047d7653253d3538df1f4399f7d5726f1705454f
Ivelin <mihajlov.ivelin@gmail.com>
2024-06-11 18:38:36 +0300

    task 2

--- dr12/index.js
+++ dr12/index.js
@@ -4,3 +4,6 @@ Array.splice(1,1,'Kiwi')
 Array.splice(2,1,'Mango')
 console.log('T1: ' + Array);
  
+/* Task 2 :You have an array of student names. Sort the array in alphabetical order. */
+let Array2 = ["John", "Anna", "Zoe", "Mark", "Beth"];
+console.log('T2: ' + Array2.sort());
