c686150d0197a6ba058c5baad5b32fab8ba0ad39
Ivelin <mihajlov.ivelin@gmail.com>
2024-06-11 18:38:40 +0300

    task 3

--- dr12/index.js
+++ dr12/index.js
@@ -7,3 +7,9 @@ console.log('T1: ' + Array);
 /* Task 2 :You have an array of student names. Sort the array in alphabetical order. */
 let Array2 = ["John", "Anna", "Zoe", "Mark", "Beth"];
 console.log('T2: ' + Array2.sort());
+
+/* Task 3 :You have an array of numbers. Create a new array that contains only the first 5 numbers from the original array. */
+let Array3 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let firstFiveArray3 = Array3.slice(0,5)
+console.log('T3 Old array: ' + Array3);
+console.log('T3 New array: ' + firstFiveArray3);
