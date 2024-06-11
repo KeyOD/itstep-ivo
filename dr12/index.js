5d10cf3734a9d232b278f9364ef0622e8d9b9b96
Ivelin <mihajlov.ivelin@gmail.com>
2024-06-11 18:38:25 +0300

    task 1

---
+++ dr12/index.js
@@ -0,0 +1,6 @@
+/* Task 1 :You have an array of fruits. Remove "Banana" and "Orange" from the array and add "Kiwi" and "Mango" in their place. */
+let Array = ["Apple", "Banana", "Orange", "Cherry"];
+Array.splice(1,1,'Kiwi')
+Array.splice(2,1,'Mango')
+console.log('T1: ' + Array);
+ 
