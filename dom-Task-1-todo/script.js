392486419835cd96a3d021ff8526399c06611266
Ivelin <mihajlov.ivelin@gmail.com>
2024-07-01 18:52:42 +0300

    added js to do job, added some styling

--- dom-Task-1-todo/script.js
+++ dom-Task-1-todo/script.js
@@ -1,4 +1,74 @@
 const app = () => {
-    
+    document.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('addTaskButton')){
+            showTask(e.target.dataset.task)
+        }
+        if(e.target.classList.contains('complete')){
+            completeOrRemoveTask(e,'complete')
+        } else if (e.target.classList.contains('remove')){
+            completeOrRemoveTask(e,'remove')
+        }
+    })
 }
 window.addEventListener('load', app)
+
+function showTask(taskNum) {
+    let li = document.createElement("li")
+    let taskList = document.querySelector("#taskList")
+    let btns = createBtns(taskNum)
+    
+    let taskName = document.createElement("h2")
+    let taskDesc = document.createElement("p")
+    switch(taskNum){
+        case "Task 1":
+            taskName.innerText="Clean room"
+            taskDesc.innerText="Clean your room, because it's messy."
+            break;
+        case "Task 2":
+            taskName.innerText="Reorder books"
+            taskDesc.innerText="Reorder your books to make them good to look at while they sit on the shelf."
+            break;
+        case "Task 3":
+            taskName.innerText="Clean house"
+            taskDesc.innerText="You have waited too long to clean your house. Now is the time!"
+            break;
+        default:
+            alert("wrong task number")
+            break;
+    }
+    
+    li.dataset.taskList = taskNum
+    li.append(taskName)
+    li.append(taskDesc)
+    li.append(btns)
+
+    taskList.appendChild(li)
+}
+
+function createBtns(taskNum){
+    let btnWrapper = document.createElement("div")
+    btnWrapper.classList.add('buttons-container')
+
+    let completeBtn = document.createElement("button")
+    completeBtn.innerText = "Complete " + taskNum
+    completeBtn.dataset.task = taskNum
+    completeBtn.classList.add('complete')
+    btnWrapper.appendChild(completeBtn)
+    
+    let removeBtn = document.createElement("button")
+    removeBtn.innerText = "Remove " + taskNum
+    removeBtn.dataset.task = taskNum
+    removeBtn.classList.add('remove')
+    btnWrapper.appendChild(removeBtn)
+
+    return btnWrapper
+}
+
+function completeOrRemoveTask(e,task){
+    let targetTask = e.target.closest('[data-task-list]')
+    if(task === "complete"){
+        targetTask.classList.add('completed')
+    } else if(task == "remove"){
+        targetTask.remove()
+    }
+}
