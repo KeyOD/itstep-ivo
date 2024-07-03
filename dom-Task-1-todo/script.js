74ce0781670080cf128144ef9db57af4b08b6f61
Ivelin <mihajlov.ivelin@gmail.com>
2024-07-03 22:07:05 +0300

    commented some old code, added new code to make logic for textarea work

--- dom-Task-1-todo/script.js
+++ dom-Task-1-todo/script.js
@@ -1,7 +1,7 @@
 const app = () => {
     document.addEventListener('click',(e)=>{
         if(e.target.classList.contains('addTaskButton')){
-            showTask(e.target.dataset.task)
+            showTask(e)
         }
         if(e.target.classList.contains('complete')){
             completeOrRemoveTask(e,'complete')
@@ -12,37 +12,62 @@ const app = () => {
 }
 window.addEventListener('load', app)
 
-function showTask(taskNum) {
-    let li = document.createElement("li")
-    let taskList = document.querySelector("#taskList")
-    let btns = createBtns(taskNum)
+// function showTask(taskNum) {
+//     let li = document.createElement("li")
+//     let taskList = document.querySelector("#taskList")
+//     let btns = createBtns(taskNum)
     
+//     let taskName = document.createElement("h2")
+//     let taskDesc = document.createElement("p")
+//     switch(taskNum){
+//         case "Task 1":
+//             taskName.innerText="Clean room"
+//             taskDesc.innerText="Clean your room, because it's messy."
+//             break;
+//         case "Task 2":
+//             taskName.innerText="Reorder books"
+//             taskDesc.innerText="Reorder your books to make them good to look at while they sit on the shelf."
+//             break;
+//         case "Task 3":
+//             taskName.innerText="Clean house"
+//             taskDesc.innerText="You have waited too long to clean your house. Now is the time!"
+//             break;
+//         default:
+//             alert("wrong task number")
+//             break;
+//     }
+    
+//     li.dataset.taskList = taskNum
+//     li.append(taskName)
+//     li.append(taskDesc)
+//     li.append(btns)
+
+//     taskList.appendChild(li)
+// }
+
+function showTask(e) {
+    e.preventDefault()
+    let li = document.createElement("li")
+    let btns = createBtns('task')
+    let taskList = document.querySelector("#taskList")
+    let textarea = document.querySelector('#textarea')
+    if(textarea.value !== ''){
+        let tasks = createTask('New Task',textarea.value)
+        for (const task of tasks) {
+            li.append(task)
+        }
+        li.append(btns)
+        taskList.appendChild(li)
+        textarea.value = ''
+    }
+}
+
+function createTask(name, description) {
     let taskName = document.createElement("h2")
     let taskDesc = document.createElement("p")
-    switch(taskNum){
-        case "Task 1":
-            taskName.innerText="Clean room"
-            taskDesc.innerText="Clean your room, because it's messy."
-            break;
-        case "Task 2":
-            taskName.innerText="Reorder books"
-            taskDesc.innerText="Reorder your books to make them good to look at while they sit on the shelf."
-            break;
-        case "Task 3":
-            taskName.innerText="Clean house"
-            taskDesc.innerText="You have waited too long to clean your house. Now is the time!"
-            break;
-        default:
-            alert("wrong task number")
-            break;
-    }
-    
-    li.dataset.taskList = taskNum
-    li.append(taskName)
-    li.append(taskDesc)
-    li.append(btns)
-
-    taskList.appendChild(li)
+    taskName.innerText=name
+    taskDesc.innerText=description
+    return [taskName,taskDesc]
 }
 
 function createBtns(taskNum){
@@ -65,7 +90,7 @@ function createBtns(taskNum){
 }
 
 function completeOrRemoveTask(e,task){
-    let targetTask = e.target.closest('[data-task-list]')
+    let targetTask = e.target.closest('li')
     if(task === "complete"){
         targetTask.classList.add('completed')
     } else if(task == "remove"){
